894205b01a76343aec7b3d724d96989f9a977eb4
usercsie <usercsie@gmail.com>
2020-02-10 21:28:11 +0800

    Complete GetFileName function

--- GoogleTestConsoleTest/PathOper.h
+++ GoogleTestConsoleTest/PathOper.h
@@ -6,18 +6,29 @@ namespace XYZCore
 	class PathOper
 	{
 	public:
-		static std::string GetFileName(std::string filePath, bool withExtension = true, char seperator = '\\')
+		static std::string GetFileName(std::string filePath, char seperator = '\\')
 		{
-			// Get last dot position
-			std::size_t dotPos = filePath.rfind('.');
 			std::size_t sepPos = filePath.rfind(seperator);
 
 			if (sepPos != std::string::npos)
 			{
-				int size = filePath.size() - (withExtension || dotPos != std::string::npos ? 1 : dotPos);
+				int size = filePath.size() - sepPos - 1;
 				return filePath.substr(sepPos + 1, size);
 			}
-			return "";
+			return filePath;
+		}
+
+		static std::string GetFileNameWithoutExtension(std::string filePath, char seperator = '\\')
+		{
+			std::string fileName = GetFileName(filePath, seperator);
+
+			std::size_t dotPos = fileName.rfind('.');
+			if (dotPos != std::string::npos)
+			{
+				return fileName.substr(0, fileName.length() - dotPos - 1);
+			}
+
+			return fileName;			
 		}
 	};
 }
--- GoogleTestConsoleTest/PathOperTest.cpp
+++ GoogleTestConsoleTest/PathOperTest.cpp
@@ -2,22 +2,63 @@
 #include "gtest/gtest.h"
 #include "PathOper.h"
 
-//TEST(PathOperTest, GetFileName_with_extension)
-//{
-//	std::string expect = "123.txt";
-//	std::string fullfileName = "D:\\ImgOut\\123.txt";
-//
-//	std::string fileName = XYZCore::PathOper::GetFileName(fullfileName);
-//
-//	EXPECT_EQ(expect, fileName);
-//}
-//
-//TEST(PathOperTest, GetFileName_without_extension)
-//{
-//	std::string expect = "123";
-//	std::string fullfileName = "D:\\ImgOut\\123.txt";
-//
-//	std::string fileName = XYZCore::PathOper::GetFileName(fullfileName, false);
-//
-//	EXPECT_EQ(expect, fileName);
-//}
+TEST(PathOperTest, GetFileName)
+{
+	std::string expect = "123.txt";
+	std::string fullfileName = "D:\\ImgOut\\123.txt";
+
+	std::string fileName = XYZCore::PathOper::GetFileName(fullfileName);
+
+	EXPECT_EQ(expect, fileName);
+}
+
+TEST(PathOperTest, GetFileName_InputOnlyFileName)
+{
+	std::string expect = "123.txt";
+	std::string fullfileName = "123.txt";
+
+	std::string fileName = XYZCore::PathOper::GetFileName(fullfileName);
+
+	EXPECT_EQ(expect, fileName);
+}
+
+TEST(PathOperTest, GetFileName_InputNoExtension)
+{
+	std::string expect = "123";
+	std::string fullfileName = "D:\\ImgOut\\123";
+
+	std::string fileName = XYZCore::PathOper::GetFileName(fullfileName);
+
+	EXPECT_EQ(expect, fileName);
+}
+
+
+TEST(PathOperTest, GetFileName_without_extension)
+{
+	std::string expect = "123";
+	std::string fullfileName = "D:\\ImgOut\\123.txt";
+
+	std::string fileName = XYZCore::PathOper::GetFileNameWithoutExtension(fullfileName);
+
+	EXPECT_EQ(expect, fileName);
+}
+
+TEST(PathOperTest, GetFileName_without_extension_InputOnlyFileName)
+{
+	std::string expect = "123";
+	std::string fullfileName = "123.txt";
+
+	std::string fileName = XYZCore::PathOper::GetFileNameWithoutExtension(fullfileName);
+
+	EXPECT_EQ(expect, fileName);
+}
+
+TEST(PathOperTest, GetFileName_without_extension_InputNoExtension)
+{
+	std::string expect = "123";
+	std::string fullfileName = "D:\\ImgOut\\123";
+
+	std::string fileName = XYZCore::PathOper::GetFileNameWithoutExtension(fullfileName);
+
+	EXPECT_EQ(expect, fileName);
+}
